5df6b953a7bb63853be6dcfa949e2b7e834c1376
John Walker <john_walker_333@hotmail.com>
2019-01-05 23:33:03 +0000

    adding assets

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+
+    public AudioClip mainTheme;
+    public AudioClip menuTheme;
+
+    string sceneName;
+
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        string newSceneName = scene.name;
+        if (newSceneName != sceneName)
+        {
+            sceneName = newSceneName;
+            Invoke("PlayMusic", .2f);
+        }
+    }
+
+
+    void PlayMusic()
+    {
+        AudioClip clipToPlay = null;
+
+        if (sceneName == "Menu")
+        {
+            clipToPlay = menuTheme;
+        }
+        else if (sceneName == "Game")
+        {
+            clipToPlay = mainTheme;
+        }
+
+        if (clipToPlay != null)
+        {
+            AudioManager.instance.PlayMusic(clipToPlay, 2);
+            Invoke("PlayMusic", clipToPlay.length);
+        }
+    }
+}
---
+++ Assets/Scripts/TextButton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TextButton : Button
+{
+
+    Text text;
+
+    protected override void Awake()
+    {
+        text = GetComponentInChildren<Text>();
+        text.color = colors.normalColor;
+        base.Awake();
+    }
+
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        text.color = colors.highlightedColor;
+        base.OnPointerEnter(eventData);
+    }
+
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        text.color = colors.normalColor;
+        base.OnPointerExit(eventData);
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        text.color = colors.pressedColor;
+        base.OnPointerDown(eventData);
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        text.color = colors.normalColor;
+        base.OnPointerUp(eventData);
+    }
+}
